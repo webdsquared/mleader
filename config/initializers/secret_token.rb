6cb28c5b41a170166be6856c6c095ac5e330d6af
Aaron Johnson <aaronjohnsonis@gmail.com>
2013-05-02 06:46:42 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mleader::Application.config.secret_token = '717763cfa32e42a8b98c237b68d50dde2cffaca095ebf40dbd70e78ced06c82212e681820a890cef6febc13d4382c23d6cee9aeeb595d35a97165b3f0832b5f8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Mleader::Application.config.session_store :cookie_store, key: '_mleader_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Mleader::Application.config.session_store :active_record_store
